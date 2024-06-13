dd02a1bd1614c195ca32d71d19db544861490aa7
Rahul Sinha <rhsinha999@gmail.com>
2024-06-13 10:54:07 +0530

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from './App.module.css'
+import Display from "./components/Display";
+import ButtonContainer from "./components/ButtonContainer";
+
+function App() {
+ 
+
+  return <div className={styles.calculator}>
+    <Display></Display>
+     <ButtonContainer></ButtonContainer>
+  </div>
+}
+
+export default App
